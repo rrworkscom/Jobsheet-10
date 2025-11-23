75b616f88b0590ac06d8b2b00acccded88d3c02d
rrworkscom <rrworks010@gmail.com>
2025-11-23 17:55:39 +0700

    Dynamic Number added

--- SIAKAD20.java
+++ SIAKAD20.java
@@ -2,31 +2,40 @@ import java.util.Scanner;
 
 public class SIAKAD20 {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
 
-        int[][] score = new int[4][3];
+        System.out.print("Enter number of students: ");
+        int studentCount = sc.nextInt();
 
-        for (int i = 0; i < score.length; i++) {
-            System.out.print("Enter scores for student #" + (i + 1));
+        System.out.print("Enter number of courses: ");
+        int courseCount = sc.nextInt();
+
+        int[][] score = new int[studentCount][courseCount];
+
+        for (int i = 0; i < studentCount; i++) {
+            System.out.println("Enter scores for student #" + (i + 1));
             double sumForEachStudent = 0;
 
-            for (int j = 0; j < score[i].length; j++) {
-                System.out.println("Course #" + (j + 1) + " score: ");
+            for (int j = 0; j < courseCount; j++) {
+                System.out.print("Course #" + (j + 1) + " score: ");
                 score[i][j] = sc.nextInt();
                 sumForEachStudent += score[i][j];
             }
 
-            System.out.println("Average score: " + sumForEachStudent / 3);
+            System.out.println("Average score: " + (sumForEachStudent / courseCount));
+            System.out.println();
+        }
 
-            for (int j = 0; j < 3; j++) {
-                double sumForEachCourse = 0;
+        System.out.println("=== Course Averages ===");
+        for (int j = 0; j < courseCount; j++) {
+            double sumForEachCourse = 0;
 
-                for (int i = 0; i < 4; i++) {
-                    sumForEachCourse += score[i][j];
-                }
-
-                System.out.println("Course " + (j + 1) + ": " + (sumForEachCourse /4));
+            for (int i = 0; i < studentCount; i++) {
+                sumForEachCourse += score[i][j];
             }
+
+            System.out.println("Course " + (j + 1) + ": " + (sumForEachCourse / studentCount));
         }
     }
 }
